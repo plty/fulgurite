6294a3529380ab8035e8c852aea940090d32ac2a
jer <jer@woof>
2024-01-28 16:44:24 +0000

    init: pnpm astro add {solid,markdoc,alpinejs,tailwind}

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,10 @@
-import { defineConfig } from 'astro/config';
+import alpine from "@astrojs/alpinejs";
+import markdoc from "@astrojs/markdoc";
+import solid from "@astrojs/solid-js";
+import tailwind from "@astrojs/tailwind";
+import { defineConfig } from "astro/config";
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  integrations: [solid(), markdoc(), alpine(), tailwind()],
+});
