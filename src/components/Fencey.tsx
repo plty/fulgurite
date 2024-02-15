f6527fd40d385c39a4043f7655236af2701f5cd1
jer <jer@woof>
2024-02-15 16:08:43 +0000

    still-compiles

--- src/components/Fencey.tsx
+++ src/components/Fencey.tsx
@@ -1,16 +1,82 @@
-export type EditorConfig = {
+import _reduce from "lodash/reduce";
+
+export const shortnames = {
+  fastIntel: [
+    "-std=c++23",
+    ...["-Wpedantic", "-Wall", "-Wextra", "-Wconversion", "-Werror"],
+    "-O3",
+    "-march=sapphirerapids",
+  ],
+  fastAmd: [
+    "-std=c++23",
+    ...["-Wpedantic", "-Wall", "-Wextra", "-Wconversion", "-Werror"],
+    "-O3",
+    "-march=znver4",
+  ],
+  asan: ["-fsanitize=address", "-fno-omit-frame-pointer"],
+  tsan: ["-fsanitize=thread"],
+  msan: ["-fsanitize=memory"],
+  ubsan: ["-fsanitize=undefined"],
+  dfsan: ["-fsanitize=dataflow"],
+  lsan: ["-fsanitize=leak"],
+} as { [k: string]: string[] };
+
+type FenceConfig = {
+  lang: string;
+  compileOpts: string[];
+  filename: string;
+  ins: [number, number][];
+  del: [number, number][];
   lineNumber: boolean;
   readonly: boolean;
 };
 
-export const defaultEditorConfig = {
+const defaultConfig = {
+  lang: "cpp",
+  compileOpts: [],
+  filename: "",
+  ins: [],
+  del: [],
   lineNumber: true,
   readonly: false,
-};
+} satisfies FenceConfig;
 
-export const normalizeEditorConfig = (
-  p: Partial<EditorConfig>,
-): EditorConfig => ({
-  ...defaultEditorConfig,
-  ...p,
-});
+export const parseInfo = (s: string) => {
+  const [langWithOpt, ...rest] = s.split(";");
+  const regex = /^(\w+)(?:\[(.*)\])?$/;
+  const [_full, lang, compileOptShorts] = regex.exec(langWithOpt) ?? [];
+  const compileOpts = compileOptShorts
+    ?.split(",")
+    .flatMap((x) => shortnames[x] ?? []);
+  return _reduce(
+    rest,
+    (acc, r) => {
+      const [k, v] = r.split("=");
+      if (k === "title") {
+        return { ...acc, filename: v };
+      } else if (k === "ins") {
+        return {
+          ...acc,
+          ins: v.split(",").map((x): [number, number] => {
+            const [start, end] = x.split("-");
+            return [+start, +end];
+          }),
+        };
+      } else if (k === "del") {
+        return {
+          ...acc,
+          del: v.split(",").map((x): [number, number] => {
+            const [start, end] = x.split("-");
+            return [+start, +end];
+          }),
+        };
+      }
+      return acc;
+    },
+    {
+      ...defaultConfig,
+      lang,
+      compileOpts,
+    } as FenceConfig,
+  );
+};
--- src/components/Foil.tsx
+++ src/components/Foil.tsx
@@ -5,7 +5,8 @@ import { jsxLanguage, tsxLanguage } from "@codemirror/lang-javascript";
 import { rustLanguage } from "@codemirror/lang-rust";
 import { StreamLanguage } from "@codemirror/language";
 import { gas } from "@codemirror/legacy-modes/mode/gas";
-import type { EditorConfig } from "./Fencey";
+import { type EditorConfig } from "./Fencey";
+import _reduce from "lodash/reduce";
 
 export const parser = {
   asm: StreamLanguage.define(gas),
@@ -30,5 +31,21 @@ export const Foil = ({ code, editorConfig, lang, lineGroup }: FoilProp) => {
       editorConfig={editorConfig}
       lineGroup={lineGroup}
     />
+  );
+};
+
+export const FramedFoil = () => {
+  return (
+    <div>
+      <div className="border-night-500 flex border-x border-t">
+        <div className="border-frost-3 bg-night-700 border-t-2 px-6 py-1 font-mono">
+          hehe.cpp
+        </div>
+        <div className="border-night-500 bg-night-600 w-full border-b border-l" />
+      </div>
+      <div className="border-night-500 border-x border-b text-sm">
+        <Foil code={content} lang={language} lineGroup={{}} editorConfig={{}} />
+      </div>
+    </div>
   );
 };
